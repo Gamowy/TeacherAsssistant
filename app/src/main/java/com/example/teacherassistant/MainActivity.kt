03f12243d678c7498f1b9092b9e51acf6b5b5899
Patryk Gamrat <Patryk.gam@gmail.com>
2024-12-27 20:59:52 +0100

    Init commit

---
+++ app/src/main/java/com/example/teacherassistant/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.teacherassistant
+
+import android.os.Bundle
+import android.view.ViewGroup
+import androidx.activity.enableEdgeToEdge
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
+import androidx.core.view.updatePadding
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.example.teacherassistant.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // Set the padding of the root view to the system insets
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemInsets = insets.getInsets(
+                WindowInsetsCompat.Type.systemBars()
+
+            )
+            val topInset = insets.getInsets(
+                WindowInsetsCompat.Type.systemBars()
+                        or WindowInsetsCompat.Type.displayCutout()
+            )
+            v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                leftMargin = systemInsets.left
+                bottomMargin = systemInsets.bottom
+                rightMargin = systemInsets.right
+            }
+            v.updatePadding(
+                top = topInset.top,
+            )
+            WindowInsetsCompat.CONSUMED
+        }
+
+        // Setup app bar
+        val appBar = binding.appbar
+        setSupportActionBar(appBar)
+        supportActionBar?.title = "Classes"
+
+        // Setup bottom navigation
+        val navView: BottomNavigationView = binding.navView
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        val navController = navHostFragment.navController
+        navView.setupWithNavController(navController)
+
+        // Setup navigation with appBar
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_classes, R.id.navigation_students, R.id.navigation_settings
+            )
+        )
+        appBar.setupWithNavController(navController, appBarConfiguration)
+    }
+}
